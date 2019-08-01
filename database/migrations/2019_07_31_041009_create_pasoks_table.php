7e6482c0f71f05b601b32c25354b06ad9137711e
mazizr <saitamasensei710@gmail.com>
2019-08-01 08:18:02 +0700

    ganti

---
+++ database/migrations/2019_07_31_041009_create_pasoks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePasoksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pasoks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('distributors_id');
+            $table->unsignedBigInteger('bukus_id');
+            $table->Integer('jumlah');
+            $table->date('tanggal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pasoks');
+    }
+}
